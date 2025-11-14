2982492b7808af0adcd4a8bb0d6cec0108b2da8d
osantosrei <reinaldo.santos37@outlook.com>
2025-11-14 19:28:02 -0300

    Initialize Localibrary API project with Maven and Spring Boot

---
+++ src/main/java/com/localibrary/LocalibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.localibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalibraryApiApplication.class, args);
+	}
+
+}
